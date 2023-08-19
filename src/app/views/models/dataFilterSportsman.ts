41842976d5383de2ec1a395fefbdc68fb9548bc6
Javier Zapata <jaalzamo@gmail.com>
2023-08-19 15:37:22 -0500

    filtros y detalles de visualizacion deportista

---
+++ src/app/views/models/dataFilterSportsman.ts
@@ -0,0 +1,33 @@
+export const jsonData = [
+    {
+      title:'Categoria',
+      property: 'category',
+      disable:false,
+      isOpen:false,
+      control:[
+        {name:'Pony', value:'Pony', code:''},
+        {name:'Amateur', value:'Amateur', code:''},
+        {name:'Juvenil', value:'juvenil', code:''},
+        {name:'Profesional', value: 'Profesional', code:''}
+      ]
+    },{
+      title:'Genero',
+      property: 'gender',
+      disable:false,
+      isOpen:false,
+      control:[
+        {name:'Femenino', value:'Femenino', code:''},
+        {name:'Masculino', value:'Masculino', code:''}
+      ]
+    },
+    {
+      title:'Tipo de identificación',
+      property: 'typeIdentification' ,
+      disable:false,
+      isOpen:false,
+      control:[
+        {name:'T. de Identidad', value:'TI', code:''},
+        {name:'C. de Ciudadania', value:'Cédula', code:''}
+      ]
+    }
+  ]
