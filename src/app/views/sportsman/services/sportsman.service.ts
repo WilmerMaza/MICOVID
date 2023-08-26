fd099aab68ed43ce44ebe34bfc3f38e71ea8ce8e
Javier Zapata <jaalzamo@gmail.com>
2023-08-26 13:30:35 -0500

    deportista, historial categorico filtro dinamico de acuerdo categorias

--- src/app/views/sportsman/services/sportsman.service.ts
+++ src/app/views/sportsman/services/sportsman.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MicoviApiService } from 'src/app/services/micovi-api.service';
 import { Sportsman } from '../../models/DataSportsman';
+import { categoryModel } from '../../models/categoryModel'
+import { HistorialCategory } from '../../models/HistorialCategoryModel'
 import { DynamicObject } from 'src/app/shared/model/filterModel';
 
 @Injectable({
@@ -17,6 +19,17 @@ export class SportsmanService {
   getSFilterSportsman(filterData:DynamicObject<any>): Observable<Sportsman[]>{
     const endpoint = '/sportMan/get';
     return this.micovid$.post(endpoint, filterData);
+  }
+
+  getAllCategory (): Observable<categoryModel[]>{
+    const endpoint = '/Categoria/getAll';
+    return this.micovid$.get(endpoint);
+  }
+
+  getHistoryCategory (idObject: { id: string }): Observable<HistorialCategory[]>{
+    const endpoint = '/sportMan/getHistorialCategory';
+    return this.micovid$.post(endpoint, idObject);
+
   }
   
 }
