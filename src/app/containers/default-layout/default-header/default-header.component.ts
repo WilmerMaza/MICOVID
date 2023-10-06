00abd54633c1240450ff4e14e609028415f683bc
Grexon-hack-2 <jsarabialugo2306@gmail.com>
2023-10-06 01:22:03 -0500

    task20231006 - detalles header

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -2,11 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 import { AuthService } from 'src/app/services/auth-service.service';
-import { Validators } from 'src/app/utils/Validators';
+import { NormaliceUpperUnicosValidators, Validators } from 'src/app/utils/Validators';
 import { DataUser } from 'src/app/views/pages/model/dataUserModel';
 import { PlansService } from 'src/app/views/pages/plans/services/plans.service';
 import { SessionService } from 'src/app/views/pages/services/session.service';
 import { objectStikyUser, stiky } from '../../utilsLayout/constants'
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-default-header',
   templateUrl: './default-header.component.html',
@@ -20,12 +22,33 @@ export class DefaultHeaderComponent extends HeaderComponent implements OnInit {
   public newNotifications = new Array(5);
   public namePerson?: string = '';
   public stickyUser: stiky | undefined;
+  public indicadoRuta:string;
   constructor(
     private classToggler: ClassToggleService,
     private session$: SessionService,
     private Auth$: AuthService,
+    private router: Router
   ) {
     super();
+
+    router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe(e => {
+      this.indicadoRuta = this.transformindicador(router.url.split("?")[0]);
+    });
+  }
+
+  transformindicador(key: string): string{
+    let routeChild = key.split("/");
+    let route = routeChild.length > 2 ? 
+    NormaliceUpperUnicosValidators.normaliceData(routeChild[2]): 
+    NormaliceUpperUnicosValidators.normaliceData(routeChild[1]);
+    switch (route) {
+      case "Sportsman":
+        return "Deportista"
+      case "Plan-anual":
+        return "Plan anual"
+      default:
+        return route;
+    }
   }
 
   ngOnInit(): void {
