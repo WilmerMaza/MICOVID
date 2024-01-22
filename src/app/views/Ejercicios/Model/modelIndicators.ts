03106e5f2106914712ac08c9779e08e824f46126
WilmerMaza <wilmermaza10@gmail.com>
2024-01-21 19:22:40 -0500

    Mejoras Indicadores

--- src/app/views/Ejercicios/Model/modelIndicators.ts
+++ src/app/views/Ejercicios/Model/modelIndicators.ts
@@ -1,45 +1,44 @@
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export class indicatorsFormModel {
-
-    formIndicators(): FormGroup {
-        return new FormGroup({
-            name:new FormControl('',[Validators.required]),
-            description: new FormControl(''),
-            levelCal: new FormControl('', [Validators.required]),
-            nameLevel: new FormControl('', [Validators.required]),
-            level: new FormControl({value:1, disabled:true}),
-            descriptionLevel: new FormControl(''),
-            absolute: new FormControl(0, [Validators.required]),
-            sportman: new FormControl('', [Validators.required]),
-            abrevt: new FormControl('', [Validators.required])
-          })
-    }
+  formIndicators(): FormGroup {
+    return new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl(''),
+      levelCal: new FormControl('', [Validators.required]),
+      nameLevel: new FormControl({ value: '', disabled: true }, [
+        Validators.required,
+      ]),
+      level: new FormControl({ value: 1, disabled: true }),
+      descriptionLevel: new FormControl({ value: null, disabled: true }),
+      absolute: new FormControl('', [Validators.required]),
+      abrevt: new FormControl('', [Validators.required]),
+    });
+  }
 }
 
 export interface IndicatorModel {
-    name: string | null;
-    description: string | null;
-    levelCal: number | null;
-    absolute: boolean | null;
-    sportman: string | null;
-    levelList: templateList[]; 
-    exercisesList: string[];
-    abrev:string;
+  name: string | null;
+  description: string | null;
+  levelCal: number | null;
+  absolute: boolean | null;
+
+  levelList: templateList[];
+  exercisesList: string[];
+  abrev: string;
 }
-  
-export  interface levelList {
-    level: number
+
+export interface levelList {
+  level: number;
 }
 
 export interface listSportMan {
-    name:string,
-    ID: string
+  name: string;
+  ID: string;
 }
 
 export interface templateList {
-    number: number,
-    name : string,
-    description:string
+  number: number;
+  name: string;
+  description: string;
 }
